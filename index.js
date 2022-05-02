227f9aa774a1842baa0d75bd612c6efe5dee6589
BasharIraqi <bashar.oroq@hotmail.com>
2022-05-02 17:39:14 +0300

    update

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express=require('express');
+const bodyParser=require('body-parser');
+const mongoClient=require('mongodb').MongoClient;
 
 const app = express();
+mongoClient.connect('mongodb://localhost:27017',(err,client)=>{
+if(err){
+   return console.log('cannot connect to mongodb');
+}
+console.log('connected to mongodb');
+});
 
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname+'/index.html');
 })
 
+app.post('/quotes', (req,res) => {
+console.log(req.body);
+});
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
